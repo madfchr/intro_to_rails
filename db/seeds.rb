ec063340c9e129ebc02637bcdd1b3a8d7b70bac0
Ariane Deschenes Foucher <ariane.df@me.com>
2016-04-06 14:38:05 -0400

    Making pictures show at /pictures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
+Picture.create!(
+ :title => "Jesus Saves",
+ :artist => "Ariane Deschenes Foucher",
+ :url => "http://40.media.tumblr.com/f6f9c2383a5ae9e2b6ecd8d6b27ae93a/tumblr_o0j0flKniV1uv9ox1o1_1280.jpg"
+)
+
+Picture.create!(
+ :title => "LA sunset",
+ :artist => "Ariane Deschenes Foucher",
+ :url => "http://40.media.tumblr.com/b9417f19addf5c7f39e06d863a8eb6a4/tumblr_o0j0dx5K5Z1uv9ox1o2_1280.jpg"
+)
+
+Picture.create!(
+  :title => "Last exam",
+  :artist => "Ariane Deschenes Foucher",
+  :url => "http://41.media.tumblr.com/35236d1083b57343d9af236fc9c58b7a/tumblr_o0j08ym1zg1uv9ox1o1_1280.jpg"
+)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
